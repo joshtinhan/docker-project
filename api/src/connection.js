bf36310919a02dd9a13d68c0a2137ecc8224ed54
Josh Chou <josh.chou@shoalter.com>
2022-11-07 15:07:54 +0800

    feat: without compose file

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://mongo:27017/mongo-test";
+// const connection = "mongodb://mongo:27017/mongo-test";
+const connection = "mongodb://172.17.0.3:27017/mongo-test";
 
 const connectDb = () => {
   return mongoose.connect(connection);
